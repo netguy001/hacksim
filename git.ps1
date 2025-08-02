75a2b2d68417de7ce5eca6577e544bab43619dfb
netguy001 <needyman001@gmail.com>
2025-08-02 22:15:56 +0530

    first commit

---
+++ git.ps1
@@ -0,0 +1,20 @@
+# Create README
+echo "# hacksim" > README.md
+
+# Initialize Git repo
+git init
+
+# Add README
+git add README.md
+
+# Commit changes
+git commit -m "first commit"
+
+# Set main branch
+git branch -M main
+
+# Add remote origin (change this if needed)
+git remote add origin https://github.com/netguy001/hacksim.git
+
+# Push to GitHub
+git push -u origin main
